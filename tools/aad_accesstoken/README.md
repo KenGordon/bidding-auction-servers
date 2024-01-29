6e021a2b53d302015af0c4d696983fba9106c1e6
beejones <ronny.bjones@hotmail.com>
2024-01-29 15:48:06 +0000

    Refactor to use env variables and use GetAccessToken

--- tools/aad_accesstoken/README.md
+++ tools/aad_accesstoken/README.md
@@ -2,6 +2,19 @@
 
 
 ```bash
-# Inside privacy-sandbox-dev directory
-make accesstoken
+# Inside demo/bidding-auction-servers directory
+1. create aad.env file
+
+Example:
+export ClientApplicationId=<your Azure Active Directory client id>
+export ClientSecret=<your Azure Active Directory client secret>
+export TenantId=<your Azure Active Directory tenant id>
+export AadEndpoint=https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token
+export ApiIdentifierUri=<your Azure Active Directory API identifier uri>
+
+2. in terminal: source ./aad.env
+
+This will export the environment variables to the shell so they can be used in test
+```
+tools/aad_accesstoken/test
 ```
--- tools/aad_accesstoken/aad_accesstoken.cc
+++ tools/aad_accesstoken/aad_accesstoken.cc
@@ -86,20 +86,114 @@ class AccessTokenTest : public testing::Test {
   // Initialize and shutdown gRPC client. DO NOT REMOVE.
 };
 
-TEST_F(AccessTokenTest, SimpleRestCallSuccess) {
-  // This test case is to demonstrate how to call a rest api
+TEST_F(AccessTokenTest, GetAccessTokenSuccess) {
+    // This test case is to demonstrate how to retrieve an accesstoken via GetAccessToken
+
   // Declare a shared pointer to an HttpClientInterface
   std::shared_ptr<google::scp::core::HttpClientInterface> http_client;
 
   // Attempt to get the Http1Client from the GlobalCpio
-  auto client = GlobalCpio::GetGlobalCpio()->GetHttp1Client(http_client);
+  GlobalCpio::GetGlobalCpio()->GetHttp1Client(http_client);
+
+  // Create an AccessTokenClientOptions instance and set its properties
+  privacy_sandbox::server_common::AccessTokenClientOptions tokenOptions;
+  tokenOptions.endpoint = absl::GetFlag(FLAGS_aad_endpoint);
+  tokenOptions.clientid = absl::GetFlag(FLAGS_client_application_id);
+  tokenOptions.clientSecret = absl::GetFlag(FLAGS_client_secret);
+  tokenOptions.apiApplicationId = absl::GetFlag(FLAGS_api_application_id);
+
+  // Create an AccessTokenFetcherManager instance
+  auto accesstoken_fetcher_factory =
+      privacy_sandbox::server_common::AccessTokenClientFactory::Create(
+          tokenOptions, http_client);
+  auto [response, body, status_code] = accesstoken_fetcher_factory->GetAccessToken();
+
+  // Check the response
+  if (status_code < 300) {
+    std::cout << "Response body: " << body << std::endl;
+    std::cout << "Status code: " << status_code << std::endl;
+  } else {
+    std::cout << "[ FAILURE ] Unexpected status code: " << status_code
+              << std::endl;
+  }
+
+  // Check the result
+  ASSERT_TRUE(response.Successful());
+  ASSERT_GT(body.length(), 0);
+  ASSERT_EQ(status_code, 200);
+}
+
+TEST_F(AccessTokenTest, RetrieveAccessTokenSuccess) {
+    // This test case is to demonstrate how to retrieve an accesstoken
+
+  // Declare a shared pointer to an HttpClientInterface
+  std::shared_ptr<google::scp::core::HttpClientInterface> http_client;
+
+  // Attempt to get the Http1Client from the GlobalCpio
+  GlobalCpio::GetGlobalCpio()->GetHttp1Client(http_client);
+
+  // Create an AccessTokenClientOptions instance and set its properties
+  privacy_sandbox::server_common::AccessTokenClientOptions tokenOptions;
+  tokenOptions.endpoint = "your_endpoint";
+  tokenOptions.clientid = absl::GetFlag(FLAGS_client_application_id);
+  tokenOptions.clientSecret = absl::GetFlag(FLAGS_client_secret);
+  tokenOptions.apiApplicationId = absl::GetFlag(FLAGS_api_application_id);
+
+  // Set http header
+  absl::btree_multimap<std::string, std::string> headers;
+  headers.insert(std::make_pair("Content-Type", "application/x-www-form-urlencoded"));
+
+  //std::string request_body = "client_id=4dcf9615-265c-40e9-93d4-728fbe1d1857&client_secret=85k8Q~k-rMytUNlQQQtF3zneNNY5B8hcl5b2Dckx&scope=api://d538182b-fc13-4cf2-a5a5-946db26caef3/.default&grant_type=client_credentials";
+  std::ostringstream request_body_stream;
+  request_body_stream << "client_id=" << absl::GetFlag(FLAGS_client_application_id)
+                      << "&client_secret=" << absl::GetFlag(FLAGS_client_secret)
+                      << "&scope=" << absl::GetFlag(FLAGS_api_application_id) << "/.default"
+                      << "&grant_type=client_credentials";
+  std::string request_body = request_body_stream.str();
+  std::cout << "Request Body: " << request_body << std::endl;
+  std::cout << "URL: " << absl::GetFlag(FLAGS_aad_endpoint) << std::endl;
+
+  // Create an AccessTokenFetcherManager instance
+  auto accesstoken_fetcher_factory =
+      privacy_sandbox::server_common::AccessTokenClientFactory::Create(
+          tokenOptions, http_client);
+  auto [response, body, status_code] = accesstoken_fetcher_factory->MakeRequest(
+      absl::GetFlag(FLAGS_aad_endpoint),
+      google::scp::core::HttpMethod::POST,
+      headers,
+      request_body
+    );
+
+  // Check the response
+  if (status_code < 300) {
+    std::cout << "Response body: " << body << std::endl;
+    std::cout << "Status code: " << status_code << std::endl;
+  } else {
+    std::cout << "[ FAILURE ] Unexpected status code: " << status_code
+              << std::endl;
+  }
+
+  // Check the result
+  ASSERT_TRUE(response.Successful());
+  ASSERT_GT(body.length(), 0);
+  ASSERT_EQ(status_code, 200);
+}
+
+TEST_F(AccessTokenTest, SimpleRestCallSuccess) {
+  // This test case is to demonstrate how to call a rest api
+
+  // Declare a shared pointer to an HttpClientInterface
+  std::shared_ptr<google::scp::core::HttpClientInterface> http_client;
+
+  // Attempt to get the Http1Client from the GlobalCpio
+  GlobalCpio::GetGlobalCpio()->GetHttp1Client(http_client);
 
   // Create an AccessTokenClientOptions instance and set its properties
   privacy_sandbox::server_common::AccessTokenClientOptions tokenOptions;
   tokenOptions.endpoint = "your_endpoint";
   tokenOptions.clientid = "your_client_id";
   tokenOptions.clientSecret = "your_client_secret";
-  tokenOptions.apiUri = "your_api_uri";
+  tokenOptions.apiApplicationId = "your_api_uri";
 
   // Create an AccessTokenFetcherManager instance
   auto accesstoken_fetcher_factory =
@@ -122,58 +216,16 @@ TEST_F(AccessTokenTest, SimpleRestCallSuccess) {
   ASSERT_GT(body.length(), 0);
   ASSERT_EQ(status_code, 200);
 }
-/**
-TEST_F(AccessTokenTest, RetrieveAccessTokenSuccess) {
-  // This test case is to demonstrate how to retrieve an accesstoken
 
-  privacy_sandbox::server_common::AccessTokenClientOptions tokenOptions;
-  tokenOptions.endpoint = "your_endpoint";
-  tokenOptions.clientid = "your_client_id";
-  tokenOptions.clientSecret = "your_client_secret";
-  tokenOptions.apiUri = "your_api_uri";
-
-  auto accesstoken_fetcher_manager =
-      privacy_sandbox::server_common::AccessTokenClientFactory::Create(
-          tokenOptions, http_client);
-
-  // Declare a shared pointer to an HttpClientInterface
-  std::shared_ptr<google::scp::core::HttpClientInterface> http_client;
-
-  // Attempt to get the Http1Client from the GlobalCpio
-  auto client = GlobalCpio::GetGlobalCpio()->GetHttp1Client(http_client);
-
-  // Check if the operation was successful
-  if (!client.Successful()) {
-    // If not successful, print an error message and return from the function
-    std::cout << "[ FAILURE ] Unable to get Http Client." << std::endl
-              << std::endl;
-    return;
-  }
-
-  http_client->Init();
-  http_client->Run();
-  auto [response, body, status_code] =
-      MakeRequest(*http_client,
-                  "https://cat-fact.herokuapp.com/facts/random?amount=1");
-    if (status_code < 300) {
-      std::cout << "Response body: " << body << std::endl;
-      std::cout << "Status code: " << status_code << std::endl;
-    } else {
-      std::cout << "[ FAILURE ] Unexpected status code: " << status_code
-                << std::endl;
-    }
-
-    // Check the result
-    ASSERT_TRUE(response.Successful());
-    ASSERT_GT(body.length(), 0);
-    ASSERT_EQ(status_code, 200);
-}
-*/
 }  // namespace
 }  // namespace privacy_sandbox::aad_accesstoken
 
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
+  for (int i = 0; i < argc; ++i) {
+    std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+  }
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
