0cc6d982d7339b3673b42d1f85a97e86fea45e48
Kapil Vaswani <kapilv@microsoft.com>
2024-10-27 23:10:34 +0000

    id

--- generateBid.js
+++ generateBid.js
@@ -1,31 +1,10 @@
 async function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
-  // Extract relevant information from signals
-  const { adMetadata } = interestGroup;
-  const { renderUrl, bidAmount } = adMetadata || {};
-
-  // Set a default bid if no bid amount is provided
-  const bid = bidAmount || 1.0;
-
-  console.log("Logging from generateBid");
-
-  // Return bid details in JSON format
   return {
-      ad: {
-          renderUrl,
-          metadata: {
-              // Metadata is optional but can be used for additional ad info
-              category: adMetadata.category || "general",
-              advertiser: adMetadata.advertiser || "unknown"
-          }
-      },
-      bid,
-      render: renderUrl,
-      adComponents: interestGroup.adComponents || [], // Optional ad components
-      allowComponentAuction: true, // If multi-level auctions are allowed
-      interestGroupName: interestGroup,
-      adCost: 1.0,
-      modellingSignals: 0,
-      bidCurrency: "EUR",
-      privateAggregationContributions: []
+    ad: {
+        renderUrl: "https://example.com/ad",
+        metadata: { category: "test" }
+    },
+    bid: 0.5,
+    render: "https://example.com/ad"
   };
 }
