0b83747538ed40fd0a99f14511fbc574ef9d1684
Kapil Vaswani <46554948+kapilvgit@users.noreply.github.com>
2025-02-09 14:00:42 +0000

    Update generateBid.js

--- generateBid.js
+++ generateBid.js
@@ -1,7 +1,6 @@
 async function generateBids(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
   return {
-    interest_group_name: interestGroup.name,
     bid: 0.5,
-    render: "https://example.com/ad" + trustedBiddingSignals[0].value
+    render: "https://example.com/ad"
   };
 }
